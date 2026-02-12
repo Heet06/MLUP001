1c054f8718efe401fc9770887774e571ca6fdf8a
Heet06 <radadiyaheet@gmail.com>
2026-02-12 22:36:19 +0530

    Added Week-1 content.

---
+++ Week-1/dir_numpy.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 22 21:29:21 2026
+
+@author: Heet
+"""
+
+import numpy
+
+contents = dir(numpy)
+print(contents)
---
+++ Week-1/getting started.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 13 19:23:14 2026
+
+@author: Heet
+"""
+
+a = 11
+b = a * 10
+print(a,b)
