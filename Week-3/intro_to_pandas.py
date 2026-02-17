45425fcd754c9147ab724bd3107b82a4a85677ca
Heet06 <radadiyaheet@gmail.com>
2026-02-18 01:17:52 +0530

    init Week-3 content and datasets.

---
+++ Week-3/intro_to_pandas.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 18 01:10:36 2026
+
+@author: Heet
+"""
+
+import os
+import pandas as pd
+import numpy as np
+
+cars_data = pd.read_csv("Datasets/Toyota.csv")
